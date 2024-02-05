2c49d58365889a104f7e788f82ecdf21a5fa4ba3
Alex Kachurin <alex.a.kachurin@gmail.com>
2024-02-05 13:47:12 -0800

    add sub mult exponential

---
+++ add_sub_mult.py
@@ -0,0 +1,38 @@
+#Given a string containing numbers, find all possible placements of +-* so that
+#resulting expression is equial to a value provided
+#Example
+#123 6 => [1*2*3, 1+2+3]
+from pdb import set_trace as b
+
+def dfs(solutions,current_solution,s,val)->list:
+    print(f"dfs {solutions} {current_solution} {s} {val}")
+    L = len(s)
+    for i in range(1,L+1):
+        solution = current_solution
+        num = int(s[0:i])
+        solution += str(num)
+        if i==L:
+            if num == val:
+                solutions.append(solution)
+        else:
+            #try +
+            solution_so_far = solution + "+"
+            solutions = dfs(solutions,solution_so_far,s[i:],val-num)
+            #try -
+            solution_so_far = solution + "-"
+            solutions = dfs(solutions,solution_so_far,s[i:],num-val)
+            #try *
+            solution_so_far = solution + "*"
+            if num >0 :
+                solutions = dfs(solutions,solution_so_far,s[i:],val/num)
+
+    return solutions
+
+def arrange_ops(s,val):
+    solutions = []
+    current_solution = ""
+    return dfs(solutions,current_solution,s,val)
+
+s = "123" 
+val = 6
+print(arrange_ops(s,val))
